65b9f31608bf79dd03ed894b0327b7423a361151
HudseyBoYouTube <ktmcmwkzpt@privaterelay.appleid.com>
2026-02-22 14:24:40 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     // This base path is CRUCIAL for GitHub Pages to find your files
-    base: '/Unblocked-Games-/', 
+    base: '/', 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
